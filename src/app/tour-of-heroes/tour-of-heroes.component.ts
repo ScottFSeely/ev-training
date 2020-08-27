0d255cbed21a39143fafaefce283d7f61dba2755
evelynpcooper <ecooper@zagmail.gonzaga.edu>
2020-08-27 11:41:56 -0700

    Styling for Page not found

--- src/app/tour-of-heroes/tour-of-heroes.component.ts
+++ src/app/tour-of-heroes/tour-of-heroes.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-tour-of-heroes',
   templateUrl: './tour-of-heroes.component.html',
-  styleUrls: ['./tour-of-heroes.component.scss']
+  styleUrls: [
+    './tour-of-heroes.component.scss', 
+    '../app.component.scss'
+  ]
 })
 export class TourOfHeroesComponent implements OnInit {
 
