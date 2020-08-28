48e0434002d4b5728b24d4b0c8c513758ad9c460
evelynpcooper <ecooper@zagmail.gonzaga.edu>
2020-08-28 11:04:08 -0700

    fixed the routerLink by adding a '/'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { FirstComponent } from './first/first.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { TourOfHeroesComponent } from './tour-of-heroes/tour-of-heroes.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 
 const routes: Routes = [
   { path: 'first', component: FirstComponent },
   { path: 'tour-of-heroes', component: TourOfHeroesComponent},
+  { path: 'heroes', component: HeroesComponent},
   { path: '**', component: PageNotFoundComponent }
 ];
 
