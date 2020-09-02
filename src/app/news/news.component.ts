5a5a20e9a36c605f563e053333ff8ae89cf47ee8
evelynpcooper <ecooper@zagmail.gonzaga.edu>
2020-09-02 11:51:12 -0700

    attempt to get the GET request to work

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+// import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 import { ApiService } from '../api.service';
+// import { MessageService } from '../message.service';
 
 
 @Component({
@@ -10,12 +13,14 @@ import { ApiService } from '../api.service';
 export class NewsComponent implements OnInit {
   articles;
 
-  constructor(private apiService: ApiService) { }
+  constructor(
+    private apiService: ApiService) { }
 
   ngOnInit() {
-    this.apiService.getNews().subscribe((data) => {
-      console.log(data);
-      this.articles = data['articles'];
+    this.apiService.getNews().subscribe(
+      (data) => {
+        console.log(data);
+        this.articles = data['articles'];
     });
   }
 
