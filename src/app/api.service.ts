bd472b824ce9bf7298f6c05234cfd243a3664058
evelynpcooper <ecooper@zagmail.gonzaga.edu>
2020-09-16 10:40:49 -0700

    small fixes

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -10,22 +10,32 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class ApiService {
   API_KEY = 'd3d3e9b9136d45c1a513dc5a7086909d';
+  news_url = 'https://newsapi.org';
   url = '/v2/top-headlines?country=us&apiKey=d3d3e9b9136d45c1a513dc5a7086909d';
+  gTr_url = "https://translation.googleapis.com";
   tr_url = "https://translation.googleapis.com/language/translate/v2";
   
   constructor( private http: HttpClient ) { }
 
   // methods
   public getNews(){
-    console.log(this.http.get(this.url)['status']);
+    const endpoint_url = this.news_url + this.url;
+    console.log(endpoint_url);
+    console.log(this.http.get(endpoint_url)['articles']);
+    console.log(this.http.get(endpoint_url));
+
 
     return this.http.get(this.url);
     // return this.http.get(`https://newsapi.org/v2/top-headlines?' + 'country=us' + '&apiKey=${this.API_KEY}`);
   }
 
   public getTr(text: string) {
-    console.log(this.http.post(this.tr_url, text));
+    console.log(this.http.post(this.gTr_url+'/language/translate/v2', text));
     return this.http.post(this.tr_url, text);
+  }
+
+  public getSupportedLang(){
+    // GET /v3/{parent=projects/*}/supportedLanguages
   }
 
   
--- src/app/gtranslate/gtranslate.component.ts
+++ src/app/gtranslate/gtranslate.component.ts
@@ -29,4 +29,5 @@ export class GTranslateComponent implements OnInit {
     this.apiService.getTr(text);
   }
 
+
 }
