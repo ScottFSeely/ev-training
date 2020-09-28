ff329b830967c19667fa6c11e08ccd01a004d320
evelynpcooper <ecooper@zagmail.gonzaga.edu>
2020-09-28 10:58:27 -0700

    removed some files and added test suites to news.component.spec.ts

--- src/app/news/news.component.spec.ts
+++ src/app/news/news.component.spec.ts
@@ -1,5 +1,7 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, inject, getTestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing'
+
 import {HttpClientModule} from '@angular/common/http';
 
 import { NewsComponent } from './news.component';
@@ -7,86 +9,169 @@ import { of } from 'rxjs';
 import { ApiService } from '../api.service';
 import { HttpClient } from '@angular/common/http';
 
+describe('NewsComponent: stub ApiService should create', () => {
+  let injector: TestBed;
+  let component: NewsComponent;
+  let fixture: ComponentFixture<NewsComponent>;
+  let service: ApiService;
+  let httpMock: HttpTestingController;
 
-describe('NewsComponent: show/hide button',() => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      declarations: [NewsComponent],
+      providers: [
+        ApiService, 
+        HttpTestingController]
+    });
+    injector = getTestBed();
+    service = injector.get(ApiService);
+    httpMock = injector.get(HttpTestingController);
 
-    let component: NewsComponent;
-    let fixture: ComponentFixture<NewsComponent>;
-    // let apiServiceStub: ApiService;
+    fixture = TestBed.createComponent(NewsComponent);
+    component = fixture.componentInstance;
 
-    const paragraphs = document.querySelectorAll("input");
-    
+    // console.log(fixture);
+    // console.log(component);
+  });
 
-    beforeEach(async(() => {
-        TestBed.configureTestingModule({
-          declarations: [ NewsComponent ],
-          imports: [ 
-            HttpClientTestingModule,
-            HttpClientModule ],
-          providers: [ 
-            { provide: ApiService, useClass: FakeApiService }
-            // { provide: HttpClient, useClass: HttpClient}
-           ]
-        })
-        .compileComponents();
-      }));
-    
-      beforeAll(() => {
-        fixture = TestBed.createComponent(NewsComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
-      });
+  afterAll(() => {
+    console.log(fixture);
+    console.log(component);
+    console.log(httpMock);
+  });
 
-      it('#fakeService should return json object', () => {
-        // expect(FakeApiService).toBe(5);
-      });
+  afterEach(() => {
+    // httpMock.verify();
+  });
 
-      it('should return faked values from fakeService', () => {
-        let index = 0;
-        const expected_titles = ["Fake article 1", "Fake article 2"]
-        // for (let article of component.getNews()) {
-        //   expect(article.title).toBe(expected_titles[index])
-        //   index++
-        // }
-      })
+  it('#injector should be Truthy', () => {
+    expect(injector).toBeTruthy();
+  });
+  it('#service should be Truthy', () => {
+    expect(service).toBeTruthy();
+  });
+  it('#httpMock should be Truthy', () => {
+    expect(httpMock).toBeTruthy();
+  });
+  it('#fixture should be Truthy', () => {
+    expect(fixture).toBeTruthy();
+  });
+  it('#component should be Truthy', () => {
+    expect(component).toBeTruthy();
+  });
+  it('#component should return articles', () => {
+    let articles = component.articles;
+    console.log(`Show me the articles! ${component.articles}`);
+    component.articles = "test";
+    console.log(`Show me the articles! ${component.articles}`);
+
+  })
+
 });
 
-class FakeApiService {
+describe('getProducts', () => {
+  let injector: TestBed;
+  let myProvider: ApiService;
+  let httpMock: HttpTestingController;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ApiService]
+    });
+    injector = getTestBed();
+    myProvider = injector.get(ApiService);
+    httpMock = injector.get(HttpTestingController);
+  });
+  it('should return an Observable<Product[]>', () => {
+    const someProducts = [
+      { id: 1, name: 'Product001', cost: 10 , quantity: 100 },
+      { id: 2, name: 'Product002', cost: 100, quantity: 200 },
+      { id: 3, name: 'Product003', cost: 200, quantity: 300 }
+    ];
+    myProvider.getProducts().subscribe((products) => {
+      expect(products.length).toBe(3);
+      expect(products).toEqual(someProducts);
+    });
+  });
+});
   
-  fakeVal = 5;
-  getNews() {
-    const fakeNewsJson = {
-      "status":"ok",
-      "totalResults":38,
-      "articles":[{
-          "source":{
-              "id":"fake-1",
-              "name":"Fake 1"
-            },
-          "author":"Chris Welch",
-          "title":"Fake article 1",
-          "description":"Fake description 1",
-          "url":"https://www.google.com",
-          "urlToImage":"https://cdn.vox-cdn.com/thumbor/ojeUgo3ftd2A4u3THNHkUL4l3rA=/0x537:4608x2950/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset/file/21903921/0420DD31_1DBC_4C2C_B4BF_1368A98D1E2D.jpeg",
-          "publishedAt":"2020-09-23",
-          "content":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-          }, {
-          "source":{
-              "id":"fake-2",
-              "name":"Fake 2"
-            },
-          "author":"E Cooper",
-          "title":"Fake article 2",
-          "description":"Fake description 2",
-          "url":"https://www.google.com",
-          "urlToImage":"https://cdn.vox-cdn.com/thumbor/ojeUgo3ftd2A4u3THNHkUL4l3rA=/0x537:4608x2950/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset/file/21903921/0420DD31_1DBC_4C2C_B4BF_1368A98D1E2D.jpeg",
-          "publishedAt":"2020-09-23",
-          "content":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-        }]
-    };  
-    return fakeNewsJson
-  }
-}
+
+// fdescribe('NewsComponent: show/hide button', () => {
+
+//     let component: NewsComponent;
+//     let fixture: ComponentFixture<NewsComponent>;
+//     let testBedService: ApiService;
+//     // let apiServiceStub: ApiService;
+
+//     const paragraphs = document.querySelectorAll("input");
+    
+
+//     beforeEach(async(() => {
+      
+//       TestBed.configureTestingModule({
+//         declarations: [ NewsComponent ],
+//         imports: [ 
+//           HttpClientTestingModule,
+//           HttpClientModule,
+//           RouterTestingModule ],
+//         providers: [ 
+//           ApiService
+//           // { provide: ApiService, useClass: FakeApiService }
+//           // { provide: HttpClient, useClass: HttpClient}
+//           ]
+//       })
+//       fakeApiService = TestBed.get(ApiService);
+//       .compileComponents();
+//     }));
+
+//     beforeEach(async(() => {
+      
+//       TestBed.configureTestingModule({
+//         declarations: [ NewsComponent ],
+//         imports: [ 
+//           HttpClientTestingModule,
+//           HttpClientModule,
+//           RouterTestingModule ],
+//         providers: [ 
+//           ApiService
+//           // { provide: ApiService, useClass: FakeApiService }
+//           // { provide: HttpClient, useClass: HttpClient}
+//           ]
+//       })
+//       fakeApiService = TestBed.get(ApiService);
+//       .compileComponents();
+//     }));
+    
+//     beforeAll(() => {
+//       fixture = TestBed.createComponent(NewsComponent);
+//       component = fixture.componentInstance;
+//       fixture.detectChanges();     
+//     });
+
+//     it('#fakeService should return json object', () => {
+//       // expect(FakeApiService).toBe(5);
+//     });
+
+//     it('should show fake service', () => {
+//       // expect(FakeApiService).toBe(5);
+//       TestBed.inject([ApiService],(injectService: ApiService));
+      
+//       console.log(`fixture looks like: ${fixture}`);
+//       console.log(`component looks like: ${component}`); 
+//     });
+
+//     it('should return faked values from fakeService', () => {
+//       let index = 0;
+//       const expected_titles = ["Fake article 1", "Fake article 2"]
+//       // for (let article of component.getNews()) {
+//       //   expect(article.title).toBe(expected_titles[index])
+//       //   index++
+//       // }
+      
+//     })
+// });
+
 
 
 // // describes the test and has a callback function
