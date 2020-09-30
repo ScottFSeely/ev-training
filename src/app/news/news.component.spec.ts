81510a4613854f6bc02549d4d5b71bef81e0605a
evelynpcooper <ecooper@zagmail.gonzaga.edu>
2020-09-30 13:32:45 -0700

    cleanup

--- src/app/news/news.component.spec.ts
+++ src/app/news/news.component.spec.ts
@@ -67,25 +67,25 @@ describe('NewsComponent', () => {
     console.log(spyFunctions);
     expect(spyFunctions).toBeDefined();
   })
-  // it('should return "test" from spy function', fakeAsync(() => {
-  //   let returnedVal: any;
-  //   let Subscription = apiService.getNews().subscribe((result) => {
-  //     returnedVal = result;
-  //   });
-  //   // expect(returnedVal).toEqual({articles: 'test'});
-  //   // expect(returnedVal['articles']).toEqual('test');
-  //   Subscription.unsubscribe(); // should this go inside of afterAll()? 
-  // }))
-  // it('should return "hello" from spy function', () => {
-  //   let hola;
-  //   console.log('Returned from getNews is: ',apiService.getNews().subscribe((result)=>{hola = result;}));
-  //   expect(hola).toEqual({articles: 'test'});
-  //   expect(hola['articles']).toEqual('test');
-  // })
+  it('should return "test" from spy function', fakeAsync(() => {
+    let returnedVal: any;
+    let Subscription = apiService.getNews().subscribe((result) => {
+      returnedVal = result;
+    });
+    expect(returnedVal).toEqual({articles: 'test'});
+    expect(returnedVal['articles']).toEqual('test');
+    Subscription.unsubscribe(); // should this go inside of afterAll()? 
+  }))
+  it('should return "hello" from spy function', () => {
+    let hola;
+    console.log('Returned from getNews is: ',apiService.getNews().subscribe((result)=>{hola = result;}));
+    expect(hola).toEqual({articles: 'test'});
+    expect(hola['articles']).toEqual('test');
+  })
 
 });
 
-describe("should test jasmine concepts", () => {
+xdescribe("should test jasmine concepts", () => {
   it("should create a spy on AFunction and use the fake function", () => {
     const fakeMessage = "I did the FAKE thing HAHA";
     var obj = new AClass();
@@ -99,61 +99,5 @@ describe("should test jasmine concepts", () => {
   })
 });
 
-// fdescribe('NewsComponent: stub ApiService should create like before', () => {
-//   let component: NewsComponent;
-//   let fixture: ComponentFixture<NewsComponent>;
-//   let service: ApiService;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       imports: [],
-//       declarations: [NewsComponent],
-//       providers: [
-//         ApiService,
-//         HttpTestingController
-//         ]
-//     }).compileComponents();
-    
-
-//     service = TestBed.inject(ApiService);
-//     fixture = TestBed.createComponent(NewsComponent);
-//     component = fixture.componentInstance;
-//   }));
-
-//   afterAll(() => {
-//     console.log(fixture);
-//     console.log(component);
-//     console.log(service);
-//     console.log(component.articles);
-//     console.log(component.getMock);
-//     console.log(component.getMock());
-//   });
-
-//   afterEach(() => {
-//   });
-
-
-//   it('#service should be Truthy', () => {
-//     expect(service).toBeTruthy();
-//   });
-//   it('#fixture should be Truthy', () => {
-//     expect(fixture).toBeTruthy();
-//   });
-//   it('#component should be Truthy', () => {
-//     expect(component).toBeTruthy();
-//   });
-//   it('#component should return articles', () => {
-//     let articles = component.articles;
-//     console.log(`Show me the articles! ${component.articles}`);
-//     component.articles = "test";
-//     console.log(`Show me the articles! ${component.articles}`);
-//   });
-//   it('should return value "test', () => {
-//     spyOn(service, "getMock");
-//     // let result = component.getNews();
-//     expect(result).toEqual('test');
-//     expect(service.getMock).toHaveBeenCalled();
-//   })
-
-// });
 
---
+++ src/app/news/news.component3.spec.ts
@@ -0,0 +1,65 @@
+import { async, ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
+import {HttpClientModule} from '@angular/common/http';
+import { of } from 'rxjs';
+
+import { NewsComponent } from './news.component';
+import { ApiService } from '../api.service';
+import { HttpTestingController } from '@angular/common/http/testing';
+
+// fdescribe('NewsComponent: stub ApiService should create like before', () => {
+//   let component: NewsComponent;
+//   let fixture: ComponentFixture<NewsComponent>;
+//   let service: ApiService;
+
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       imports: [],
+//       declarations: [NewsComponent],
+//       providers: [
+//         ApiService,
+//         HttpTestingController
+//         ]
+//     }).compileComponents();
+    
+
+//     service = TestBed.inject(ApiService);
+//     fixture = TestBed.createComponent(NewsComponent);
+//     component = fixture.componentInstance;
+//   }));
+
+//   afterAll(() => {
+//     console.log(fixture);
+//     console.log(component);
+//     console.log(service);
+//     console.log(component.articles);
+//     console.log(component.getMock);
+//     console.log(component.getMock());
+//   });
+
+//   afterEach(() => {
+//   });
+
+
+//   it('#service should be Truthy', () => {
+//     expect(service).toBeTruthy();
+//   });
+//   it('#fixture should be Truthy', () => {
+//     expect(fixture).toBeTruthy();
+//   });
+//   it('#component should be Truthy', () => {
+//     expect(component).toBeTruthy();
+//   });
+//   it('#component should return articles', () => {
+//     let articles = component.articles;
+//     console.log(`Show me the articles! ${component.articles}`);
+//     component.articles = "test";
+//     console.log(`Show me the articles! ${component.articles}`);
+//   });
+//   it('should return value "test', () => {
+//     spyOn(service, "getMock");
+//     // let result = component.getNews();
+//     expect(result).toEqual('test');
+//     expect(service.getMock).toHaveBeenCalled();
+//   })
+
+// });
