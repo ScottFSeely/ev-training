de09838085b6a2adb778474f5379ba1175f38e24
evelynpcooper <ecooper@zagmail.gonzaga.edu>
2020-09-30 10:23:53 -0700

    add js functions and remove subscription in component

--- src/app/news/news.component.spec.ts
+++ src/app/news/news.component.spec.ts
@@ -6,27 +6,95 @@ import { NewsComponent } from './news.component';
 import { ApiService } from '../api.service';
 import { HttpTestingController } from '@angular/common/http/testing';
 
-describe(`NewsComponent: stub ApiService should create using Scott's code`, () => {
+function FakeFunction() {
+  this.testThing1 = ''
+}
+
+// my notes
+let myAdd1 = function (x,y):number {
+  return x+y;
+}
+let myAdd2: (x:number, y:number) => number = function (
+  x: number,
+  y: number
+):number {
+  return x+y;
+};
+let myAdd3 = (function(a:number, b:number){
+  console.log(a+b)
+  return a+b;
+})(1,2);
+// recursion
+function sumOfSquares(num: number):number {
+  let sum: number = Math.pow(num,2);
+  if(num==1) {
+    return 1
+  }else{
+    return sum + sumOfSquares(num-1) // calls itself
+  }
+}
+// nesting functions
+function convertToChicken(name: string) {
+  function getChickenName(name: string) {
+    return `Chicken ${name}`
+  }
+  return getChickenName(name);
+  // 1. call convertToChicken(name)
+  // 2. name goes into getChickenName(name) which is defined inside the function
+  // 3. goes into `Chicken ${name}` gets returned.
+  // can this function be used outside the function it is defined in?
+} convertToChicken('Alfred');
+// let and const are block-scoped
+function convertToChickenAgain(name:string){
+  let originalName = name;
+  function getChickenName(newName:string){
+    console.log(`originalName will still work in this scope. let and const can go in here. \n Original Name: ${originalName}`);
+    return `Chicken ${newName}`;
+  }
+  return getChickenName(name);
+} convertToChickenAgain("Peanut");
+// function in object
+const bird = {
+  chirp: function() {
+    // bird object will go in ${this}???
+    console.log('chirp', this);
+  }
+}
+const dog = {
+  bark(){
+    // dog object will go in ${this}???
+    console.log('bark', this);
+  }
+}
+const cat = {
+  meow: () => {
+    // will give an error bc ${this} is a globalThis value
+    console.log('meow', this);
+  }
+}
+
+
+
+describe('NewsComponent', () => {
   let component: NewsComponent;
   let fixture: ComponentFixture<NewsComponent>;
-  let apiService: ApiService;
 
-  describe('NewsComponent', () => {
-    let component: NewsComponent;
-    let fixture: ComponentFixture<NewsComponent>;
-    let apiService: jasmine.SpyObj<ApiService>;
-    beforeEach(async(() => {
-      const spy = jasmine.createSpyObj('ApiService', ['getNews']);
-      TestBed.configureTestingModule({
-        declarations: [NewsComponent],
-        imports: [HttpClientModule],
-        providers: [{ provide: ApiService, useValue: spy }],
-      }).compileComponents();
-      apiService = TestBed.inject(ApiService) as jasmine.SpyObj<ApiService>;
-      apiService.getNews.and.returnValue(of({}));
-      fixture = TestBed.createComponent(NewsComponent);
-      component = fixture.componentInstance;
-      fixture.detectChanges();
+  let apiService: jasmine.SpyObj<ApiService>;
+  beforeEach(async(() => {
+    const spy = jasmine.createSpyObj('ApiService', ['getNews']);
+    // const testSpy1 = jasmine.createSpy('ApiService', ['getTranslations']);
+
+    TestBed.configureTestingModule({
+      declarations: [NewsComponent],
+      imports: [HttpClientModule],
+      // replace ApiService dependency with a spy 
+      providers: [{ provide: ApiService, useValue: spy }],
+    }).compileComponents();
+    apiService = TestBed.inject(ApiService) as jasmine.SpyObj<ApiService>;
+    apiService.getNews.and.returnValue(of({}));
+    fixture = TestBed.createComponent(NewsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   }));
 
   afterAll(() => {
@@ -50,6 +118,11 @@ describe(`NewsComponent: stub ApiService should create using Scott's code`, () =
   });
   it('#service method should be called', () => {
     expect(apiService.getNews).toHaveBeenCalled();
+  })
+  it('should spy on somethings', () => {
+    let spyFunctions = spyOnAllFunctions(component);
+    console.log(spyFunctions);
+    expect(spyFunctions).toBeDefined();
   })
 
 });
